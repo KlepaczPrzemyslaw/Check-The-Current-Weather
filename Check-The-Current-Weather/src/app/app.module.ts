5c885fd1b20ff409d3f0aa8bedc2958ef62f500b
Przemek K Bitbucket <przemyslaw.klepacz.contact@gmail.com>
2020-06-25 15:11:26 +0200

    i18n + base structure

--- Check-The-Current-Weather/src/app/app.module.ts
+++ Check-The-Current-Weather/src/app/app.module.ts
@@ -3,14 +3,33 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import { HomePageComponent } from './home-page/home-page.component';
+import { WeatherResultComponent } from './weather-result/weather-result.component';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    WeatherResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
